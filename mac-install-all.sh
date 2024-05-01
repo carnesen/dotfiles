c581d8dbee26c41c27c5f02b1b1c0150c762a8cf
Chris Arnesen <chris.arnesen@gmail.com>
2024-04-30 22:08:00 -0700

    Improvements to mac-install-all for new Mac

--- mac-install-all.sh
+++ mac-install-all.sh
@@ -3,45 +3,11 @@
 set -eo pipefail
 
 # Manual configuration:
-# - https://www.cnet.com/how-to/7-macos-display-settings-to-help-you-see-your-mac-better/
+# - Decrease display size https://www.cnet.com/how-to/7-macos-display-settings-to-help-you-see-your-mac-better/
+# - Fix trackpad and keyboard settings
+# - Install 1Password manually for security sake
+
 # This script sets up a new Mac
-
-if command -v brew > /dev/null 2>&1; then
-	echo "Homebrew: already installed"
-else
-	echo "Installing Homebrew"
-	echo "Note this also installs XCode command-line tools"
-	echo "You'll be prompted to enter your admin password"
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-if [ -d "/usr/local/etc/bash_completion.d" ]; then
-	echo "Homebrew bash completion: already installed"
-else
-	echo "Installing bash completion"
-	brew install bash-completion
-fi
-
-if [ -d "/Applications/Firefox.app" ]; then
-	echo "Firefox: already installed"
-else
-	echo "Installing Firefox"
-	brew install --cask firefox
-fi
-
-if [ -d "/Applications/Google Chrome.app" ]; then
-	echo "Google Chrome: already installed"
-else
-	echo "Installing Google Chrome"
-	brew install --cask google-chrome
-fi
-
-if [ -d "/Applications/iTerm.app" ]; then
-	echo "iTerm2: already installed"
-else
-	echo "Installing iTerm2"
-	brew install --cask iterm2
-fi
 
 SHELL_DESIRED="/bin/bash"
 if [ "${SHELL}" == "${SHELL_DESIRED}" ]; then
@@ -53,6 +19,96 @@ else
 	sudo shutdown -r now
 fi
 
+BASH_PROFILE_PATH="$HOME/.bash_profile"
+if [ -f "${BASH_PROFILE_PATH}" ]; then
+	echo "~/.bash_profile already exists"
+else
+	echo "Writing ~/.bash_profile"
+	echo "" >> "${BASH_PROFILE_PATH}"
+fi
+
+if command -v brew > /dev/null 2>&1; then
+	echo "Homebrew: already installed"
+else
+	echo "Installing Homebrew"
+	echo "Note this also installs XCode command-line tools"
+	echo "You'll be prompted to enter your admin password"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+if command -v wget > /dev/null 2>&1; then
+	echo "wget: already installed"
+else
+	echo "wget: installing"
+	brew install wget
+fi
+
+if [ -d "/Applications/Google Chrome.app" ]; then
+	echo "Google Chrome: already installed"
+else
+	echo "Installing Google Chrome"
+	brew install --cask google-chrome
+fi
+
+if [ -f ~/.ssh/id_rsa ]; then
+	echo "Found SSH key"
+else
+	echo "Setting up SSH key"
+	ssh-keygen -t rsa -b 4028 -f ~/.ssh/id_rsa
+	echo "Add the following SSH public key to GitHub and rerun this script"
+	echo ""
+	cat ~/.ssh/id_rsa.pub
+	exit 0
+fi
+
+if [ -d "/usr/local/etc/bash_completion.d" ]; then
+	echo "Homebrew bash completion: already installed"
+else
+	echo "Installing bash completion"
+	brew install bash-completion
+fi
+
+if [ -d "/Applications/Moom.app" ]; then
+	echo "Moom: already installed"
+else
+	echo "Moom: installing"
+	brew install --cask moom
+fi
+
+if [ -d "/Applications/Docker.app" ]; then
+	echo "Docker Desktop: already installed"
+else
+	echo "Docker Desktop: skip installing"
+	# brew install --cask docker
+	# Launch Docker Desktop to complete the installation
+	# open "/Applications/Docker.app"
+fi
+
+if [ -d "/Applications/Slack.app" ]; then
+	echo "Slack: already installed"
+else
+	echo "Slack: skip installing"
+	# brew install --cask slack
+fi
+
+if command -v fnm > /dev/null 2>&1; then
+	echo "Fast Node Manager (fnm): already installed"
+else
+	echo "Fast Node Manager (fnm): installing"
+	brew install fnm
+	echo 'eval $(fnm env)' >> ~/.bash_profile
+	source ~/.bash_profile
+fi
+
+if command -v node > /dev/null 2>&1; then
+	echo "Node.js: already installed"
+else
+	echo "Node.js: installing"
+	fnm install 20
+	fnm default 20
+	source ~/.bash_profile
+fi
+
 GITHUB_CARNESEN_DIR="${HOME}/GitHub/carnesen"
 GITHUB_CARNESEN_DEV_DIR="${HOME}/GitHub/carnesen/dev"
 
@@ -60,9 +116,10 @@ if [ -d "${GITHUB_CARNESEN_DEV_DIR}" ]; then
 	echo "@carnesen/dev: already exists"
 else
 	echo "@carnesen/dev: installing"
+	mkdir -p "${GITHUB_CARNESEN_DIR}"
 	git -C "${GITHUB_CARNESEN_DIR}" clone git@github.com:carnesen/dev.git
 	npm --prefix "${GITHUB_CARNESEN_DEV_DIR}" ci
-	npm --prefix "${GITHUB_CARNESEN_DEV_DIR}" run dev -- remotes clone
+	npm --prefix "${GITHUB_CARNESEN_DEV_DIR}" run dev -- clone-carnesens
 fi
 
 DOTFILES_DIR="${GITHUB_CARNESEN_DIR}/dotfiles"
@@ -73,34 +130,12 @@ else
 	exit 1
 fi
 
-BASH_PROFILE_PATH="$HOME/.bash_profile"
-if [ -f "${BASH_PROFILE_PATH}" ]; then
-	echo "~/.bash_profile already exists"
+if grep "dotfiles" ~/.bash_profile; then
+	echo "dotfiles: already being sourced"
 else
-	echo "Writing ~/.bash_profile"
-	echo ". \"${DOTFILES_DIR}/.bash_profile\"" > "${BASH_PROFILE_PATH}"
-fi
-
-if [ -d "/Applications/Moom.app" ]; then
-	echo "Moom: already installed"
-else
-	brew install --cask moom
-fi
-
-if [ -d "/Applications/Docker.app" ]; then
-	echo "Docker Desktop: already installed"
-else
-	echo "Docker Desktop: installing"
-	brew install --cask docker
-	# Launch Docker Desktop to complete the installation
-	open "/Applications/Docker.app"
-fi
-
-if [ -d "/Applications/Slack.app" ]; then
-	echo "Slack: already installed"
-else
-	echo "Slack: installing"
-	brew install --cask slack
+	echo "dotfiles: sourcing from bash_profile"
+	echo "source ${DOTFILES_DIR}/.bash_profile" >> ~/.bash_profile
+	source ~/.bash_profile
 fi
 
 if command -v code > /dev/null 2>&1; then
@@ -110,6 +145,8 @@ else
 	brew install --cask visual-studio-code
 	# Launch Visual Studio Code so that it creates its directories
 	code "${DOTFILES_DIR}"
+	echo "Sleeping to make sure the program is up and running"
+	sleep 5
 fi
 
 VS_CODE_SETTINGS_FILE_PATH="${HOME}/Library/Application Support/Code/User/settings.json"
@@ -137,21 +174,6 @@ for VS_CODE_EXTENSION in ${VS_CODE_EXTENSIONS_DESIRED[@]}; do
 		code --install-extension "${VS_CODE_EXTENSION}"
 	fi
 done
-
-if command -v fnm > /dev/null 2>&1; then
-	echo "Fast Node Manager (fnm): already installed"
-else
-	echo "Fast Node Manager (fnm): installing"
-	brew install Schniz/tap/fnm
-fi
-
-if command -v node > /dev/null 2>&1; then
-	echo "Node.js: already installed"
-else
-	echo "Node.js: installing"
-	fnm install latest
-	fnm default latest
-fi
 
 if command -v gcloud > /dev/null 2>&1; then
 	echo "Google Cloud SDK: already installed"
@@ -200,13 +222,6 @@ else
 	brew install --cask github
 fi
 
-if command -v wget > /dev/null 2>&1; then
-	echo "wget: already installed"
-else
-	echo "wget: installing"
-	brew install wget
-fi
-
 if command -v gpg > /dev/null 2>&1; then
 	echo "GnuPG: already installed"
 else
@@ -214,4 +229,5 @@ else
 	brew install gnupg
 fi
 
-git config pull.rebase true
+echo "Setting git pull to rebase instead of merge by default"
+git config --global pull.rebase true
